cad008e5ee3e3b6bd2f54a0325cb7a3179252f03
angiegeralis <ageralis@haverford.edu>
2021-09-07 16:01:35 -0400

    part4

--- Student.py
+++ Student.py
@@ -1,46 +1,52 @@
 '''
 TODO: add header here
 '''
-
 class Student:
 
     """ ========== TODO : START ========== """
 
     """TODO: Create a constructor."""
-    def __init__(self, name, year, college):
-        student_name = self.name
-        student_year = self.year
-        student_college = self.college
-        student_classes = []
+    def __init__(self, student_name, student_year, student_college):
+        self.name = student_name
+        self.year = student_year
+        self.college = student_college
+        self.classes = student_classes = []
     """TODO: Write getters. Although not necessary in python, it is good
     practice to have getter and setter methods (as needed) for the sake of
     encapsulazation."""
-    def get_student_name():
+    def get_student_name(self):
         return self.name
-    def get_student_year():
+    def get_student_year(self):
         return self.year
-    def get_student_college():
-        return self.College
-
+    def get_student_college(self):
+        return self.college
+    def get_student_classes(self):
+        return self.classes
     """TODO: Write the add course method"""
-    def add_course(self, class):
-        for i in range len(self.classes):
-            if i == class:
-                print("Already enrolled")
+    def add_course(self, course):
+        # check through list of classes, if you already have that class,
+        # tell the user that you are already enrolled. If not, add the class
+        # to the list
+        for i in range(len(self.classes)):
+            if i == course:
+                print("Already enrolled. You cannot add the same class twice.")
             else:
-                self.classes.append(class)
+                self.classes.append(course)
 
     """TODO: Write the drop course method"""
     def drop_course(self, course):
+        # check through the list of classes, if you find the class that matches
+        # the parameter, remove the class. If not, tell the user they are not enrolled
         for i in range(len(self.classes)):
-            if i == class:
-                self.classes.remove(class)
+            if i == course:
+                self.classes.remove(course)
             else:
-                print("Not enrolled")
+                print("Not enrolled. You cannot remove a class you don't take.")
 
     """TODO: Write the __str__ method"""
-    def __str__:
-        print("Student: %s , Year: %i , College: %i , Classes: %a" % name, year, college, classes)
+    def __str__(self):
+        s = "Student: %s , Year: %i , College: %s , Classes: %a" % (self.name, self.year, self.college, self.classes)
+        return s
 
     """ ========== TODO : END ========== """
 
@@ -51,16 +57,20 @@ def main():
 
     print("Student Class Testing\n")
     # TODO create instance
-    angie = Student("Angie",2024,Haverford)
+    bob = Student("Bob",2024,"Haverford")
     print("Testing the getters:")
     # TODO test getters
+    print(bob.get_student_name())
+    print(bob.get_student_year())
+    print(bob.get_student_college())
 
     print("\nTesting add_course and drop_course:")
     # TODO test add/drop
-    angie.add_course(angie,"CMSC260")
-    angie.add_course(angie,"CMSC260")
+    bob.add_course("CMSC260")
+    bob.add_course("CMSC260")
     print("\nTest __str__:")
     # TODO test __str__
+    print(bob)
 
 
     student_lst = [["Fiona Xu", 2022, "Haverford College"],
